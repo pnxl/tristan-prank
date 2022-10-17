cac804cf0072fcc5a8ecbb0315b5e2da33687a97
jason <pnxl@pm.me>
2022-10-17 19:30:24 +0700

    make window always on top

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 from sound import Sound
+from ctypes import windll
 
 pygame.init()
 
 bgClr = (0, 0, 0)
 scr = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 sound = pygame.mixer.Sound("C:\Windows\Media\Windows Message Nudge.wav")
+setWindowPos = windll.user32.SetWindowPos
+
+NOSIZE = 1
+NOMOVE = 2
+TOPMOST = -1
+NOT_TOPMOST = -2
+
+
+def alwaysOnTop(boolean):
+    zorder = (NOT_TOPMOST, TOPMOST)[boolean]
+    hwnd = pygame.display.get_wm_info()['window']
+    setWindowPos(hwnd, zorder, 0, 0, 0, 0, NOMOVE | NOSIZE)
+
 
 pygame.display.set_caption('You are an idiot!')
 scr.fill(bgClr)
