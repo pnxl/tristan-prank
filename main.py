0ee73b0ac362d787792c1ca7a58ba5419dc134a3
jason <pnxl@pm.me>
2022-10-17 19:27:02 +0700

    sound go brr

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+from sound import Sound
 
 pygame.init()
 
-bgClr = (0,0,0)
+bgClr = (0, 0, 0)
 scr = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 sound = pygame.mixer.Sound("C:\Windows\Media\Windows Message Nudge.wav")
 
@@ -18,3 +19,11 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pygame.mixer.Sound.play(sound)
+        if event.type == pygame.WINDOWSHOWN:
+            if Sound.is_muted() == True:
+                Sound.mute()
+                Sound.volume_max()
+                continue
+            if Sound.is_muted() == False:
+                Sound.volume_max()
+                continue
